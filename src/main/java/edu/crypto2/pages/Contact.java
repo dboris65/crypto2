7bef5c66407cebca299e48254c4a43b05b0ea748
Boris Damjanovic, 230/08, FON, Belgrade <Boris Damjanovic, 230/08, FON, Belgrade@localhost>
2009-10-31 12:05:15 +0100

    miising Contact from previous repo

---
+++ src/main/java/edu/crypto2/pages/Contact.java
@@ -0,0 +1,11 @@
+/**
+ * Boris Damjanovic, 230/08, FON, Belgrade - crypto2 - 2009
+ */
+package edu.crypto2.pages;
+
+/***********************************************************************
+ * 
+ */
+public class Contact {
+
+}
